6af6964494fd05389205e31750ecdd804acde511
Maxim Talanov <mail@maxtalanov.ru>
2021-05-18 03:23:06 +0300

    fix(11): Исправить ошибку (обязательная)
    Можно избавиться от лишних обращений к props, используя деструктуризацию. function Card({ onCardClick, ... }) https://learn.javascript.ru/destructuring-assignment#destrukturizatsiya-obekta

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,38 +2,37 @@ import React from "react";
 
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
-function Card(props) {
-  // console.log(props, 'Компонен: CARD');
+function Card({ onClickCard, onCardLike, onCardDelete, ownerId, _id, likes, src, alt, title}) {
 
   const currentUser = React.useContext(CurrentUserContext);
-  const isOwn = props.ownerId === currentUser._id;
+  const isOwn = ownerId === currentUser._id;
   const cardDeleteBtn = (
     `trash ${isOwn ? 'trash_btn_visible' : 'trash_btn_hidden'}`
   );
-  const isLiked = props.likes.some(i => i._id === currentUser._id);
+  const isLiked = likes.some(i => i._id === currentUser._id);
   const cardLikeBtnActive = `card__btn-like ${isLiked ? 'card__btn-like_active' : ''}`;
 
   function clickImg() {
-    props.onClickCard(props);
+    onClickCard({ src, title, alt });
   }
   function clickLike() {
-    props.onCardLike(props);
+    onCardLike({ likes });
   }
   function removeCard() {
-    props.onCardDelete(props);
+    onCardDelete({ _id });
   }
 
   return(
     <figure className="card">
       <button className={`${cardDeleteBtn} hover-opacity`} onClick={removeCard} />
-      <img className="card__img" src={props.src} alt={props.alt}  onClick={clickImg}/>
+      <img className="card__img" src={src} alt={alt}  onClick={clickImg}/>
 
       <figcaption className="card__bottom">
         <div className="card__text">
-          <p className="card__title">{props.title}</p>
+          <p className="card__title">{title}</p>
           <div className="card__like-box">
             <button className={`card__btn-like ${cardLikeBtnActive} hover-opacity`} type="button" onClick={clickLike}/>
-            <p className="card__like-text">{props.likes.length} </p>
+            <p className="card__like-text">{likes.length} </p>
           </div>
         </div>
       </figcaption>
